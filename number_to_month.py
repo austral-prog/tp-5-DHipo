0029ce1af64b983ac6e27dfd26d257d296cc10f7
DHipo <104331457+DHipo@users.noreply.github.com>
2024-04-23 15:16:23 -0300

    exersice months

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,14 @@
 # Replace the "ANSWER HERE" for your answer
 
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+    if month > 12 or month <= 0:
+        return "error"
+    
+    months = [
+        "enero", "febrero", "marzo", 
+        "abril", "mayo", "junio", 
+        "julio", "agosto", "septiembre",
+        "octubre", "noviembre", "diciembre"
+    ]
+    
+    return months[month-1] # Remove this line and implement
